d043564f2b0683624a79f9ab0212881852bf34f7
starling888888 <mukudori88888@gmail.com>
2026-07-04 08:56:53 +0900

    feat: add base path config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://starling888888.github.io",
+  base: "/neon-underrealm-trpg",
   integrations: [mdx()],
 });
