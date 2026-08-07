5ceeafee0c0877cf80b22389e7e8df55c8b3491b
starling888888 <mukudori88888@gmail.com>
2026-08-07 21:02:29 +0900

    test: capture GM guide menu views

--- tests/vrt/site-menu.spec.ts
+++ tests/vrt/site-menu.spec.ts
@@ -1,4 +1,7 @@
 import { siteRoutes } from "../support/site";
 import { registerVrtScenarios } from "./helpers/vrt";
 
-registerVrtScenarios("site-menu", [{ route: siteRoutes.world }]);
+registerVrtScenarios("site-menu", [
+  { route: siteRoutes.world },
+  { id: "gm", route: siteRoutes.gm },
+]);
